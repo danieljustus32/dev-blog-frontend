ab640d28fc117f8e5872c1d0f0cfe4ad8d86913f
danieljustus32 <daniel.justus32@gmail.com>
2020-02-10 23:26:33 -0600

    Add loading wheel

--- src/components/Query/index.js
+++ src/components/Query/index.js
@@ -6,7 +6,13 @@ const Query = ({ children, query, id }) => {
         variables: { id: id }
     })
 
-    if (loading) return <p>Loading...</p>
+    if (loading) {
+        return (
+            <svg className="loading-wheel-viewbox" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <circle className="loading-wheel" cx="50" cy="50" r="45" />
+            </svg>
+        )
+    }
     if (error) return <p>Error: {JSON.stringify(error)}</p>
     return children({ data })
 }
