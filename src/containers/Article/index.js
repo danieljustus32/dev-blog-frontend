fcb1d0a82f73788870182532017413e073f6e716
Daniel Justus <djustus@wgu.edu>
2020-11-30 19:35:41 -0600

    Add disqus

--- src/containers/Article/index.js
+++ src/containers/Article/index.js
@@ -1,59 +1,76 @@
-import React from "react"
-import { useParams } from "react-router"
-import Query from "../../components/Query"
-import ReactMarkdown from "react-markdown"
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import Moment from "react-moment"
-import NotFound from "../../components/404/404"
-
-import ARTICLE_QUERY from "../../queries/article/article"
+import React from "react";
+import { useParams } from "react-router";
+import Query from "../../components/Query";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { okaidia } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Moment from "react-moment";
+import NotFound from "../../components/404/404";
+import Disqus from "disqus-react";
+import ARTICLE_QUERY from "../../queries/article/article";
 
 const Article = () => {
-  let { id } = useParams()
+  let { id } = useParams();
 
   const renderers = {
-    code: ({language, value}) => {
-      return <SyntaxHighlighter style={okaidia} language={language} children={value} />
-    }
-  }
+    code: ({ language, value }) => {
+      return (
+        <SyntaxHighlighter
+          style={okaidia}
+          language={language}
+          children={value}
+        />
+      );
+    },
+  };
+
+  const disqusShortname = "djustus";
+  const disqusConfig = {
+    url: process.env.REACT_APP_FRONTEND_URL,
+    identifier: id,
+  };
 
   return (
     <Query query={ARTICLE_QUERY} id={id}>
       {({ data: { article } }) => {
         if (article === null) {
-          return <NotFound />
-        }
-        else {       
+          return <NotFound />;
+        } else {
           return (
             <div className="page-content">
               <>
-              <h1 id="article-page-title">{article.Title}</h1>
+                <h1 id="article-page-title">{article.Title}</h1>
                 <div
                   id="banner"
                   className="uk-height-medium uk-flex uk-flex-center uk-flex-middle 
                   uk-cover-background uk-light uk-padding"
                 >
-                  <img 
-                    src={article.Image[0].url}
-                    alt={article.Alt} 
-                  />
+                  <img src={article.Image[0].url} alt={article.Alt} />
                 </div>
                 <div className="uk-section">
                   <div className="uk-container uk-container-small">
-                    <ReactMarkdown source={article.Content} renderers={renderers}/>
-                    <p>
-                      <Moment format="MMM Do YYYY">{article.published_at}</Moment>
+                    <ReactMarkdown
+                      source={article.Content}
+                      renderers={renderers}
+                    />
+                    <p className="article-date">
+                      <Moment format="MMM Do YYYY">
+                        {article.published_at}
+                      </Moment>
                     </p>
+                    <Disqus.DiscussionEmbed
+                      shortname={disqusShortname}
+                      config={{ title: article.Title, ...disqusConfig }}
+                    />
                   </div>
                 </div>
               </>
-          </div>
-        )
+            </div>
+          );
         }
       }}
     </Query>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
